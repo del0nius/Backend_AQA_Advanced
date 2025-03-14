f8a2213d424aff2d9a0eccc5f8b6b8ab60773473
Daniil Gaponenko <dp.gaponenko@gmail.com>
2025-03-15 00:43:10 +0700

    Adding test for change of user's email

--- test_put_v1_account_token.py
+++ test_put_v1_account_token.py
@@ -4,12 +4,12 @@ from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
 
 
-def test_post_v1_account():
+def test_put_v1_account_token():
     # User registration
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
-    login = 'd.gaponenko_test26'
+    login = 'd.gaponenko_test27'
     email = f'{login}@mail.ru'
     password = '123456789'
     json_data = {
@@ -38,18 +38,6 @@ def test_post_v1_account():
     print(response.status_code)
     print(response.text)
     assert response.status_code == 200, f"Couldn't activate user {login}"
-
-    # Sign in
-    json_data = {
-        'login': login,
-        'password': password,
-        'rememberMe': True,
-    }
-
-    response = login_api.post_v1_account_login(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
-    assert response.status_code == 200, f"Couldn't sign in user {login}"
 
 
 def get_activation_token_by_login(
