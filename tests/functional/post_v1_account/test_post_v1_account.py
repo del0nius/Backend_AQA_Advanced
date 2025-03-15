009a231ea574690fa7b9a3d462362f512bad9125
Daniil Gaponenko <dp.gaponenko@gmail.com>
2025-03-15 18:25:10 +0700

    Add logging client & curl

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -2,14 +2,24 @@ import json
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
+import structlog
 
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(
+            indent=4,
+            ensure_ascii=True,
+            # sort_keys=True
+        )
+    ]
+)
 
 def test_post_v1_account():
     # User registration
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
-    login = 'd.gaponenko_test28'
+    login = 'd.gaponenko_test30'
     email = f'{login}@mail.ru'
     password = '123456789'
     json_data = {
