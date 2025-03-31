85607e9aeca2736aa6822da7b3a433150043b6cd
Daniil Gaponenko <dp.gaponenko@gmail.com>
2025-03-31 19:19:33 +0700

    Created facade

---
+++ helpers/account_helper.py
@@ -0,0 +1,68 @@
+import json
+
+from services.dm_api_account import DMApiAccount
+from services.api_mailhog import MailHogApi
+
+
+class AccountHelper:
+    def __init__(
+            self,
+            dm_account_api: DMApiAccount,
+            mailhog: MailHogApi
+    ):
+        self.dm_account_api = dm_account_api
+        self.mailhog = mailhog
+
+    def register_new_user(
+            self,
+            login: str,
+            password: str,
+            email: str
+    ):
+        json_data = {
+            'login': login,
+            'email': email,
+            'password': password,
+        }
+
+        response = self.dm_account_api.account_api.post_v1_account(json_data=json_data)
+        assert response.status_code == 201, f"Couldn't create user {response.json()}"
+        response = self.mailhog.mailhog_api.get_api_v2_messages()
+        assert response.status_code == 200, f"Couldn't get emails {response.json()}"
+        token = self.get_activation_token_by_login(login=login, response=response)
+        assert token is not None, f"Couldn't get token for user {login}"
+        response = self.dm_account_api.account_api.put_v1_account_token(token=token)
+        assert response.status_code == 200, f"Couldn't activate user {login}"
+        return response
+
+    def user_login(
+            self,
+            login: str,
+            password: str,
+            remember_me: bool = True
+    ):
+        json_data = {
+            'login': login,
+            'password': password,
+            'rememberMe': remember_me,
+        }
+
+        response = self.dm_account_api.login_api.post_v1_account_login(json_data=json_data)
+        assert response.status_code == 200, f"Couldn't sign in user {login}"
+        return response
+
+    @staticmethod
+    def get_activation_token_by_login(
+            login,
+            response
+    ):
+        token = None
+        for i in response.json()['items']:
+            try:
+                user_data = json.loads(i['Content']['Body'])
+                user_login = user_data['Login']
+                if user_login == login:
+                    token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+            except (json.JSONDecodeError, KeyError):
+                continue
+        return token
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,9 +1,10 @@
 import json
-from dm_api_account.apis.account_api import AccountApi
-from dm_api_account.apis.login_api import LoginApi
-from api_mailhog.apis.mailhog_api import MailhogApi
+
+from helpers.account_helper import AccountHelper
 from restclient.configuration import Configuration as MailhogConfiguration
 from restclient.configuration import Configuration as DmApiConfiguration
+from services.dm_api_account import DMApiAccount
+from services.api_mailhog import MailHogApi
 import structlog
 import time
 
@@ -22,66 +23,13 @@ def test_post_v1_account():
     mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
 
-    account_api = AccountApi(configuration=dm_api_configuration)
-    login_api = LoginApi(configuration=dm_api_configuration)
-    mailhog_api = MailhogApi(configuration=mailhog_configuration)
+    account = DMApiAccount(configuration=dm_api_configuration)
+    mailhog = MailHogApi(configuration=mailhog_configuration)
 
-    login = 'd.gaponenko_test48'
+    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
+
+    login = 'd.gaponenko_test53'
     email = f'{login}@mail.ru'
     password = '123456789'
-    json_data = {
-        'login': login,
-        'email': email,
-        'password': password,
-    }
-
-    response = account_api.post_v1_account(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
-    assert response.status_code == 201, f"Couldn't create user {response.json()}"
-
-    # Get email from email server, wait 3 seconds before execution to ensure that email is received
-    time.sleep(3)
-    response = mailhog_api.get_api_v2_messages()
-    print(response.status_code)
-    print(response.text)
-    assert response.status_code == 200, f"Couldn't get emails {response.json()}"
-
-    # Get token from email
-    token = get_activation_token_by_login(login, response)
-    assert token is not None, f"Couldn't get token for user {login}"
-
-    # User activation
-    response = account_api.put_v1_account_token(token=token)
-    print(response.status_code)
-    print(response.text)
-    assert response.status_code == 200, f"Couldn't activate user {login}"
-
-    # Sign in
-    json_data = {
-        'login': login,
-        'password': password,
-        'rememberMe': True,
-    }
-
-    response = login_api.post_v1_account_login(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
-    assert response.status_code == 200, f"Couldn't sign in user {login}"
-
-
-def get_activation_token_by_login(
-        login,
-        response
-        ):
-    token = None
-    for i in response.json()['items']:
-        try:
-            user_data = json.loads(i['Content']['Body'])
-            user_login = user_data['Login']
-            if user_login == login:
-                token = user_data['ConfirmationLinkUrl'].split('/')[-1]
-        except (json.JSONDecodeError, KeyError):
-            continue
-
-    return token
+    account_helper.register_new_user(login=login, password=password, email=email)
+    account_helper.user_login(login=login, password=password)
